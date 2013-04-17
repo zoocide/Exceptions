ca23ca20271f63902eb4835fabe39420803a1a0e
Alexander Smirnov <zoocide@gmail.com>
2013-04-18 00:48:49 +0400

    add lib/Exceptions/*

---
+++ lib/Exceptions/Exception.pm
@@ -0,0 +1,25 @@
+package Exceptions::Exception;
+use overload '""' => sub{ $_[0]->msg."\n" };
+
+=head1 DESCRIPTION
+
+It is the base class for exceptions.
+
+=cut
+
+sub new
+{
+  my $self = bless {}, shift;
+  $self->init(@_);
+  $self
+}
+
+sub msg { $_[0]{msg} }
+
+sub init
+{
+  $_[0]{msg} = defined $_[1] ? $_[1] : '';
+}
+
+1;
+
